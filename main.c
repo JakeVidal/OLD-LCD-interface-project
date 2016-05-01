30030228340451485026c2ac30084e130c6fc112
JakeVidal <jrvidal@shaw.ca>
2016-05-01 12:25:21 -0700

    fixed serial_com and made minor changes

--- main.c
+++ main.c
@@ -2,18 +2,29 @@
 #include <msp430.h>
 #include "header.h"
 
+static const uint8_t clk_pin = 0b00000001;
+static const uint8_t data_pin = 0b00000010;
+static const uint8_t latch_pin = 0b00000100;
+static const uint8_t switch_pin = 0b00001000;
+
 // Main program function
 int main(void) {
-    // Stop watchdog timer
+
+    // Stop watchdog timer to prevent timeout reset
     WDTCTL = WDTPW | WDTHOLD;
-    // Initialize P1.0, P1.1 and P1.2 to ouput as clk_pin, data_pin and latch_pin and P1.3 as input
-    P1DIR |= 0b0111;
-    // Enable interrupt on P1.3
-    P1IE |= ~0b1000;
-    // Set interrupt to trigger on high/low edge (for button)
-    P1IES |= 0b1000;
-    // Clear interrupt flag for P1.3
-    P1IFG &= ~0b1000;
+
+    // Initialize P1.0, P1.1 and P1.2 to output as clk_pin, data_pin and latch_pin then enable pullup resistors
+    P1DIR |= (data_pin | clk_pin | latch_pin);
+    P1REN |= (data_pin | clk_pin | latch_pin);
+
+    // set P1.3 as input, enable pullup resistors, enable interrupt, set to trigger on high/low edge (for button to work) and clear interrupt flags
+    P1DIR &= ~switch_pin;
+    P1REN |= switch_pin;
+    P1IE |= switch_pin;
+    P1IES |= switch_pin;
+    P1IFG = 0b00000000;
+
+
     // Loop forever with Interrupts enabled
     __enable_interrupt();
     while(1){}
@@ -22,12 +33,16 @@ int main(void) {
 // Port 1 Interrupt service routine
 #pragma vector = PORT1_VECTOR
 __interrupt void Port_1(void){
+
 	// Set latch pin to low
-	P1OUT &= ~0b0100;
+	P1OUT &= ~latch_pin;
+
 	// Outputs an 8-bit value in serial through P1.1
-	serial_com(0b0010, 0b0001, 0b10101100);
+	serial_com(data_pin, clk_pin, 0b10101100);
+
 	// Set latch pin to high
-	P1OUT |= 0b0100;
+	P1OUT |= latch_pin;
+    
 	// Clear interrupt flag for P1.3
-	P1IFG &= ~0b1000;
+	P1IFG &= ~switch_pin;
 }
--- serial_com.c
+++ serial_com.c
@@ -4,19 +4,22 @@
 
 //Function to allow serial communication with shift register
 void serial_com(uint8_t data_pin, uint8_t clk_pin, uint8_t value){
+
 	// Definitions of variables
-	uint8_t bytemask = 0b0001, k;
-	// Loop that iterates through value to send it 
+	uint8_t bytemask = 0b00000001, k;
+
+	// Loop that iterates through value to send it
 	for(k = 8; k > 0; k--){
+
 		// Set data_pin to output current 1/8th of value
 		P1OUT &= !data_pin;
-		P1OUT |= (value & bytemask);
-		// Set clk_pin to high
+		P1OUT |= (value & bytemask) >> (7 - k);
+
+		// Set clk_pin to high then back to low in order to send the data to the register
 		P1OUT ^= clk_pin;
-		// Set clk_pin to low (default)
 		P1OUT ^= clk_pin;
+
 		// Increment output to next 1/8th of value
 		bytemask <<= 1;
 	}
 }
-
