e7eb726b5dfd9a1c3e32d3d7400dff68e142c43e
JakeVidal <jrvidal@shaw.ca>
2017-02-22 21:29:24 -0800

    Further outline of LCD functions

--- LCD_interface.c
+++ LCD_interface.c
@@ -6,3 +6,11 @@
 void LCD_init(void){
 
 }
+
+void send_data(void){
+
+}
+
+void send_cmd(void){
+	
+}
--- header.h
+++ header.h
@@ -4,6 +4,8 @@
 
 //Function prototypes and type definitions
 void serial_com(uint8_t data_pin, uint8_t clk_pin, uint8_t value);
-void LCD_init(void)
+void LCD_init(void);
+void send_data(void);
+void send_cmd(void);
 
 #endif
